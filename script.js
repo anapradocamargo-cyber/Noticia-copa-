062c6effbf012a6bbd6e08ddb789d537eda0ed4f
anapradocamargo-cyber <ana.prado.camargo@escola.pr.gov.br>
2026-06-26 09:58:12 -0300

    vvv

---
+++ script.js
@@ -0,0 +1,12 @@
+function comemorar(){
+
+    const msg = document.getElementById("mensagem");
+
+    msg.innerHTML = `
+        🎆🇧🇷 HEXA! 🇧🇷🎆 <br>
+        🎉 Viva o Brasil! 🎉
+    `;
+
+    document.body.style.background =
+    "linear-gradient(135deg,#009c3b,#ffdf00,#002776,#00ff99,#ffee00)";
+}
